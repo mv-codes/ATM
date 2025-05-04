026353010f4a70fb0928ddb4e9bbfd26e7a04e48
mv-codes <mvachhani@student.umgc.edu>
2025-05-03 21:50:17 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        Account firstAccount = new Account("Bob", "000123");
+        firstAccount.showMenu();
+
+
+
+    }
+}
